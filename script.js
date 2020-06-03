1b48c5dc115865527724420aa6206662d573b50d
petitcroco <58482685+p3titcr0c0@users.noreply.github.com>
2020-06-03 19:53:21 +0200

    v1.2.0

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+function ajaxPost(url, data, callback, isJson) {
+    var req = new XMLHttpRequest();
+    req.open("POST", url);
+    req.addEventListener("load", function () {
+        if (req.status >= 200 && req.status < 400) {
+            // Appelle la fonction callback en lui passant la réponse de la requête
+            callback(req.responseText);
+        } else {
+            console.error(req.status + " " + req.statusText + " " + url);
+        }
+    });
+    req.addEventListener("error", function () {
+        console.error("Erreur réseau avec l'URL " + url);
+    });
+    if (isJson) {
+        // Définit le contenu de la requête comme étant du JSON
+        req.setRequestHeader("Content-Type", "application/json");
+        // Transforme la donnée du format JSON vers le format texte avant l'envoi
+        data = JSON.stringify(data);
+    }
+    req.send(data);
+}
+/* envoyer les datas a un script .php et tout envoyer sur la bdd */
+
+
 myDataLast = [];
 
 class MyApp {
@@ -26,12 +51,6 @@ class MyApp {
   }
 }
 
-function validation(myData) {
-	if (myData[3].length<2) {
-		console.log("incorrect !");
-	}
-}
-
 function comparaison(myDataFirst, myDataLast) {
 	myDataFirst.toString();
 	myDataLast.toString();
@@ -44,9 +63,13 @@ function commun_event() {
 	myData.listing();
 	myDataFirst = myData.output();
 	if (comparaison(myDataFirst, myDataLast) == false) {
-		console.log(myDataFirst);
 		myDataLast = myDataFirst;
-		validation(myDataFirst);
+		if (myDataFirst[3].length>2) {
+			console.log(myDataFirst);
+			var monJSON = JSON.stringify(myDataFirst);
+			ajaxPost("https://www.php.net/manual/fr/tutorial.firstpage.php", monJSON, function(){console.log('impex')}, true);
+			console.log("all is good")
+		}
 	}
 }
 
